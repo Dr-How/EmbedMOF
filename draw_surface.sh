d37b24e7b9bca76f682e829de714b078464adebb
Hao Chen <hao.chen.math@gmail.com>
2025-09-18 07:56:32 +0800

    remove useless outputs

--- draw_surface.sh
+++ draw_surface.sh
@@ -5,7 +5,7 @@ if ! grep -qx "$sym" trivial-faces.dat; then
   exit 1
 fi
 python3 Halfedges.py "$sym" >/dev/null 2>&1
-python3 DFSNormal.py #>/dev/null 2>&1
-python3 MakeFaces.py #>/dev/null 2>&1
-python3 DrawSurface.py #>/dev/null 2>&1
+python3 DFSNormal.py >/dev/null 2>&1
+python3 MakeFaces.py >/dev/null 2>&1
+python3 DrawSurface.py >/dev/null 2>&1
 rm -f *.json
